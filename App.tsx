29e695f352be0150d8d7d694c64a10baabd0c6b6
treforged <tre@treforged.com>
2026-03-25 23:32:46 -0400

    fix: update import path for ErrorBoundary in App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+// Main imports
+import React from 'react';
+import ErrorBoundary from '@/components/shared/ErrorBoundary';
+
+function App() {
+    return (
+        <ErrorBoundary>
+            {/* Your application code here */}
+        </ErrorBoundary>
+    );
+}
+
+export default App;
