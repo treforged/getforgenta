6834a5d2d4b2652d4d54ae0bc2970903d2c888de
TRE Forged <tre@treforged.com>
2026-04-20 23:58:30 -0400

    mobile ui updates

--- src/pages/BudgetControl.tsx
+++ src/pages/BudgetControl.tsx
@@ -1036,6 +1036,7 @@ export default function BudgetControl() {
   </p>
 </div>
       </div>
+      </div>
 
       {/* KPI Summary + Remaining Cash On Hand */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4">
