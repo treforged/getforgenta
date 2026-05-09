67eec399a27bdd57eb197f6fe3feca9cc0e50ab8
TRE Forged <tre@treforged.com>
2026-05-08 20:08:49 -0400

    [fix]: move AuthSessionPlugin into AppDelegate.swift so Xcode compiles it

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import AuthenticationServices
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,43 +8,88 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+    func applicationWillResignActive(_ application: UIApplication) {}
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+    func applicationWillTerminate(_ application: UIApplication) {}
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
+}
 
+// MARK: - AuthSession Plugin
+// Wraps ASWebAuthenticationSession so JS can start an OAuth flow and receive
+// the callback URL directly — no appUrlOpen / custom scheme interception needed.
+
+@objc(AuthSessionPlugin)
+public class AuthSessionPlugin: CAPPlugin, CAPBridgedPlugin {
+    public let identifier = "AuthSessionPlugin"
+    public let jsName = "AuthSession"
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(name: "start", returnType: CAPPluginReturnPromise)
+    ]
+
+    private var authSession: ASWebAuthenticationSession?
+    private var contextProvider: _AuthSessionContextProvider?
+
+    @objc func start(_ call: CAPPluginCall) {
+        guard
+            let urlString = call.getString("url"),
+            let url = URL(string: urlString),
+            let scheme = call.getString("callbackURLScheme")
+        else {
+            call.reject("Missing url or callbackURLScheme")
+            return
+        }
+
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+
+            self.authSession = ASWebAuthenticationSession(
+                url: url,
+                callbackURLScheme: scheme
+            ) { callbackURL, error in
+                if let err = error as? ASWebAuthenticationSessionError,
+                   err.code == .canceledLogin {
+                    call.reject("User cancelled")
+                    return
+                }
+                if let error {
+                    call.reject(error.localizedDescription)
+                    return
+                }
+                guard let callbackURL else {
+                    call.reject("No callback URL received")
+                    return
+                }
+                call.resolve(["url": callbackURL.absoluteString])
+            }
+
+            self.contextProvider = _AuthSessionContextProvider(bridge: self.bridge)
+            self.authSession?.presentationContextProvider = self.contextProvider
+            self.authSession?.prefersEphemeralWebBrowserSession = false
+            self.authSession?.start()
+        }
+    }
+}
+
+private class _AuthSessionContextProvider: NSObject, ASWebAuthenticationPresentationContextProviding {
+    weak var bridge: CAPBridgeProtocol?
+
+    init(bridge: CAPBridgeProtocol?) {
+        self.bridge = bridge
+    }
+
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return bridge?.viewController?.view.window ?? UIWindow()
+    }
 }
